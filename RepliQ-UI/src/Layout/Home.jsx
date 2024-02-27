e730513ac020cdd0952a7941a5f95a02d76df137
riyaz <riyaz3582@gmail.com>
2024-02-27 13:53:13 +0600

    created register page and saved data to database

--- RepliQ-UI/src/Layout/Home.jsx
+++ RepliQ-UI/src/Layout/Home.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../Authentication/Authprovider";
+import LogIn from "../Authentication/LogIn";
 
 const Home = () => {
+    const {user} = useContext(AuthContext)
+
     return (
         <div className="lg:grid grid-cols-2 ">
             <div>
                 <div className='flex items-center'>
                     
                 </div>
+                
+                    <LogIn/>
+                
             </div>
         </div>
     );
