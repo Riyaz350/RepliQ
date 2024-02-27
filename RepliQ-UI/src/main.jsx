290c225aa57daaea690cfbe553b3c5eee9697ad7
riyaz <riyaz3582@gmail.com>
2024-02-27 12:29:36 +0600

    added react router in main.js

--- RepliQ-UI/src/main.jsx
+++ RepliQ-UI/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Home from './Layout/Home.jsx';
+import Register from './Authentication/Register.jsx';
+import LogIn from './Authentication/LogIn.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/logIn",
+        element:<LogIn/>
+      },
+    ],
+  },
+  {
+    path:'register',
+    element:<Register/>
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
